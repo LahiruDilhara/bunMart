f46b7a421d8d003eebe469bd1c96371ba39b2ff0
lahirudilhara <galahirudilhara@gmail.com>
2026-01-30 21:38:18 +0530

    initialize the cart service

---
+++ backend/cartService/src/test/java/com/nsbm/bunmart/cart/BunMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.bunmart.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
