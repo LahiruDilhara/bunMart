b0535794411722c178c3013446e326eb32cecf1d
Supuni Madushani <119606198+MadushaniDGS@users.noreply.github.com>
2026-02-06 22:07:53 +0530

    Added the Kitchen Service

---
+++ backend/kitchenService/src/test/java/com/nsbm/bunmart/kitchen/BunMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.bunmart.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
