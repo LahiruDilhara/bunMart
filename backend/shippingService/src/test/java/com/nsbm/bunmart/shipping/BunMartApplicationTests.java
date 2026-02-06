ee1def71c6cf55b43560fb2eecb6b4d7482f5df6
KanishkaArochana <kanishkaarochana23@gmail.com>
2026-02-06 22:01:13 +0530

    Add the Shipping Service

---
+++ backend/shippingService/src/test/java/com/nsbm/bunmart/shipping/BunMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.bunmart.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
