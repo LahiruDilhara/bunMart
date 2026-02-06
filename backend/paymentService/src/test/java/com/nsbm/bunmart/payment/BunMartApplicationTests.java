a1075220969d5602342cb5257380813356a73e29
hiruni <hmkooragodage7@gmail.com>
2026-02-06 23:45:08 +0530

    add payment service

---
+++ backend/paymentService/src/test/java/com/nsbm/bunmart/payment/BunMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.bunmart.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
