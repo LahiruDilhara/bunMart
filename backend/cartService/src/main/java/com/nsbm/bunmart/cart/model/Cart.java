db00ab88854a2f84847f7f036f69982c317e4a1e
lahirudilhara <galahirudilhara@gmail.com>
2026-02-07 19:05:17 +0530

    added the getters and setters to the models along with constructors

--- backend/cartService/src/main/java/com/nsbm/bunmart/cart/model/Cart.java
+++ backend/cartService/src/main/java/com/nsbm/bunmart/cart/model/Cart.java
@@ -17,4 +17,37 @@ public class Cart {
 
     @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,orphanRemoval = true,fetch = FetchType.EAGER)
     private List<CartItem> cartItems = new ArrayList<>();
+
+    public Cart() {
+    }
+
+    public Cart(Integer id, String userId, List<CartItem> cartItems) {
+        this.id = id;
+        this.userId = userId;
+        this.cartItems = cartItems;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
 }
--- backend/cartService/src/main/java/com/nsbm/bunmart/cart/model/CartItem.java
+++ backend/cartService/src/main/java/com/nsbm/bunmart/cart/model/CartItem.java
@@ -18,4 +18,46 @@ public class CartItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id",nullable = false)
     private Cart cart;
+
+    public CartItem() {
+    }
+
+    public CartItem(Integer id, String productId, Integer quantity, Cart cart) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.cart = cart;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
