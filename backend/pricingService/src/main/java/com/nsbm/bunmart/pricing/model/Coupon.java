c05b44bd4a5bc31e80a4d211cb2aeda7f9f4d8a6
Kalpani Kapuge <kalpanikapuge1020@gmail.com>
2026-03-01 23:36:28 +0530

    Implement price rule repository

---
+++ backend/pricingService/src/main/java/com/nsbm/bunmart/pricing/model/Coupon.java
@@ -0,0 +1,4 @@
+package com.nsbm.bunmart.pricing.model;
+
+public class Coupon {
+}
---
+++ backend/pricingService/src/main/java/com/nsbm/bunmart/pricing/repositories/PriceRuleRepository.java
@@ -0,0 +1,11 @@
+package com.nsbm.bunmart.pricing.repositories;
+
+import com.nsbm.bunmart.pricing.model.PriceRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface PriceRuleRepository extends JpaRepository<PriceRule, String> {
+    List<PriceRule> findByProductIdInAndIsActiveTrue(List<String> productIds);
+    Optional<PriceRule> findByProductIdAndIsActiveTrue(String productId);
+}
